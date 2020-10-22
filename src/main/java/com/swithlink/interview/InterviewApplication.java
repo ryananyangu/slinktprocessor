23e74817e8fd2745a2eb6343dee21a3b8c9ad376
ryananyangu <ryananyangu@gmail.com>
2020-10-22 14:27:06 +0300

    model wrapper as bean

--- src/main/java/com/swithlink/interview/InterviewApplication.java
+++ src/main/java/com/swithlink/interview/InterviewApplication.java
@@ -1,7 +1,9 @@
 package com.swithlink.interview;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InterviewApplication {
@@ -9,6 +11,11 @@ public class InterviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InterviewApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+	   return new ModelMapper();
 	}
 
 
